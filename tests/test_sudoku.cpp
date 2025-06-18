3afd7c34994ba02dfb56e2165ac07c543802615b
Oliver Forral <intrepion@gmail.com>
2025-06-17 21:15:40 -0700

    Switched to doctest.

--- tests/test_sudoku.cpp
+++ tests/test_sudoku.cpp
@@ -1,17 +1,19 @@
-#include <gtest/gtest.h>
+#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
+
+#include <doctest.h>
+
 #include <nlohmann/json.hpp>
 #include "../src/lib/sudoku.hpp"
 
 using nlohmann::json;
 
-TEST(IsValidSudokuContent, GivenNoInitialGridPropertyShouldBeFalse) {
+TEST_CASE("IsValidSudokuContent returns false for json with no initial_grid property") {
   // Arrange
-  const json content = R"(
-)";
+  const json content = R"({})";
 
   // Act
   const bool actual = is_valid_sudoku_content(content);
 
   // Assert
-  EXPECT_FALSE(actual);
+  CHECK_FALSE(actual);
 }
